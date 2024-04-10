3f36fecfc8ace011eba7269f3aea25bfd790c89f
Andreea1011 <andreea10111@gmail.com>
2024-04-10 19:48:57 +0300

    Finished

--- main.c
+++ main.c
@@ -1,44 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 
-struct AddressTmpl
-{
+#define COLUMN_LENGTH 20
+
+struct AddressTmpl {
     char sStreet[51];
     char sCity[51];
     char sCountry[51];
 };
 
-enum Gender
-{
-    Male     = 1,
-    Female   = 2
+enum Gender {
+    Male = 1,
+    Female = 2
 };
 
-struct PersonTmpl
-{
+struct PersonTmpl {
     int nPersonID;
-
     char chRecordType;
+
 #define REC_TYPE_PERSONAL  1
 #define REC_TYPE_HOME      2
 #define REC_TYPE_WORK      3
 
-    union
-    {
+    union {
         struct /* record type 1 */
         {
             char sFirstName[31];
             char sLastName[32];
             char sBirthdate[11]; /* YYYY/MM/DD date format */
             enum Gender theGender;
-        }PersonalInfo;
+        } PersonalInfo;
 
         struct /* record type 2 */
         {
             struct AddressTmpl homeAddress;
             char sPhone[31];
             char sEmail[51];
-        }HomeDetails;
+        } HomeDetails;
 
         struct /* record type 3 */
         {
@@ -47,109 +45,87 @@ struct PersonTmpl
             char sPhone[31];
             char sFax[31];
             char sEmail[51];
-        }WorkDetails;
-    }Details;
+        } WorkDetails;
+    } Details;
 } __attribute__((packed));
 
-
-void citireFisierTest(char* inputFile, char* outputFile)
-{
-    FILE *fisier_test = fopen("testc.ini", "r");
+void citireSetari(char *inputFile, char *outputFile) {
+    FILE *fisier_configurare = fopen("testc.ini", "r");
     char line[256];
     int inSection = 0;
 
-    if(!fisier_test){
+    if (!fisier_configurare) {
         printf("Eroare");
         return;
     }
 
-    while (fgets(line, sizeof(line), fisier_test) != NULL){
+    while (fgets(line, sizeof(line), fisier_configurare) != NULL) {
         line[strcspn(line, "\r\n")] = 0;
 
-        if(strcmp(line, "[testc]") == 0)
-            inSection = 1;
+        if (strcmp(line, "[testc]") == 0) inSection = 1;
 
-        if(inSection == 1){
-            if(strncmp(line, "in=", 3) == 0)
+        if (inSection == 1) {
+            if (strncmp(line, "in=", 3) == 0) {
                 strcpy(inputFile, line + 3);
-            else{
-                if(strncmp(line, "out=", 4) == 0)
-                    strcpy(outputFile, line + 4);
+            } else if (strncmp(line, "out=", 4) == 0) {
+                strcpy(outputFile, line + 4);
             }
         }
     }
 
-    fclose(fisier_test);
-}
-
-int nrCifre(int n)
-{
-    if(n == 0)
-        return 0;
-    else
-        return 1 + nrCifre(n / 10);
-}
-
-void afisareSpatii(int n, FILE *outputFile)
-{
-    int i;
-    for(i = 0; i < n; i++)
-        fprintf(outputFile, " ");
+    fclose(fisier_configurare);
 }
 
 int main() {
     char inputFile[256], outputFile[256];
 
-    citireFisierTest(inputFile, outputFile);
+    citireSetari(inputFile, outputFile);
 
     FILE *fisier_binar = fopen(inputFile, "rb");
     FILE *fisier_ascii = fopen(outputFile, "w");
 
-    char spatiu[31] = {' '};
-
     struct PersonTmpl person;
-    size_t readSize;
 
-    while((readSize = fread(&person, sizeof(struct PersonTmpl), 1, fisier_binar)) == 1){
+    while ((fread(&person, sizeof(struct PersonTmpl), 1, fisier_binar)) == 1) {
         switch (person.chRecordType) {
             case REC_TYPE_PERSONAL:
-                fprintf(fisier_ascii, "%d", person.nPersonID);
-                afisareSpatii(4 - nrCifre(person.nPersonID), fisier_ascii);
-                fprintf(fisier_ascii, "%c", person.chRecordType);
-                fprintf(fisier_ascii, "%s", person.Details.PersonalInfo.sLastName);
-                afisareSpatii(30 - strlen(person.Details.PersonalInfo.sLastName), fisier_ascii);
-                fprintf(fisier_ascii, "%s", person.Details.PersonalInfo.sFirstName);
-                afisareSpatii(30 - strlen(person.Details.PersonalInfo.sFirstName), fisier_ascii);
-                fprintf(fisier_ascii, "%s", person.Details.PersonalInfo.sBirthdate);
-                afisareSpatii(10 - strlen(person.Details.PersonalInfo.sBirthdate), fisier_ascii);
-                fprintf(fisier_ascii, "%c\n", person.Details.PersonalInfo.theGender == Male ? 'M' : 'F');
-            break;
+                fprintf(fisier_ascii, "%*d%*d %-*.*s%-*.*s %-*.*s %c\n",
+                        0, person.nPersonID,
+                        4, person.chRecordType,
+                        30, COLUMN_LENGTH, person.Details.PersonalInfo.sLastName,
+                        30, COLUMN_LENGTH, person.Details.PersonalInfo.sFirstName,
+                        10, COLUMN_LENGTH, person.Details.PersonalInfo.sBirthdate,
+                        person.Details.PersonalInfo.theGender == Male ? 'M' : 'F');
+                break;
 
             case REC_TYPE_HOME:
-                fprintf(fisier_ascii, "%d,%c %s,%s,%s,%s,%s\n",
-                         person.nPersonID,
-                         person.chRecordType,
-                         person.Details.HomeDetails.homeAddress.sStreet,
-                         person.Details.HomeDetails.homeAddress.sCity,
-                         person.Details.HomeDetails.homeAddress.sCountry,
-                         person.Details.HomeDetails.sPhone,
-                         person.Details.HomeDetails.sEmail);
+                fprintf(fisier_ascii,
+                        "%*d% *d %-*.*s%-*.*s%-*.*s%-*.*s%-*.*s\n",
+                        0, person.nPersonID,
+                        4, person.chRecordType,
+                        30, COLUMN_LENGTH, person.Details.HomeDetails.homeAddress.sStreet,
+                        30, COLUMN_LENGTH, person.Details.HomeDetails.homeAddress.sCity,
+                        30, COLUMN_LENGTH, person.Details.HomeDetails.homeAddress.sCountry,
+                        15, COLUMN_LENGTH, person.Details.HomeDetails.sPhone,
+                        30, COLUMN_LENGTH, person.Details.HomeDetails.sEmail);
                 break;
 
             case REC_TYPE_WORK:
-                fprintf(fisier_ascii, "%d,%d,%s,%s,%s,%s,%s,%s,%s\n",
-                         person.nPersonID,
-                         person.chRecordType,
-                         person.Details.WorkDetails.sCompany,
-                         person.Details.WorkDetails.workAddress.sStreet,
-                         person.Details.WorkDetails.workAddress.sCity,
-                         person.Details.WorkDetails.workAddress.sCountry,
-                         person.Details.WorkDetails.sPhone,
-                         person.Details.WorkDetails.sFax,
-                         person.Details.WorkDetails.sEmail);
+                fprintf(fisier_ascii,
+                        "%*d% *d %-*.*s%-*.*s%-*.*s%-*.*s%-*.*s%-*.*s%-*.*s\n",
+                        0, person.nPersonID,
+                        4, person.chRecordType,
+                        30, COLUMN_LENGTH, person.Details.WorkDetails.sCompany,
+                        30, COLUMN_LENGTH, person.Details.WorkDetails.workAddress.sStreet,
+                        30, COLUMN_LENGTH, person.Details.WorkDetails.workAddress.sCity,
+                        30, COLUMN_LENGTH, person.Details.WorkDetails.workAddress.sCountry,
+                        15, COLUMN_LENGTH, person.Details.WorkDetails.sPhone,
+                        15, COLUMN_LENGTH, person.Details.WorkDetails.sFax,
+                        30, COLUMN_LENGTH, person.Details.WorkDetails.sEmail);
                 break;
 
-            default: break;
+            default:
+                break;
         }
     }
 
@@ -158,5 +134,3 @@ int main() {
 
     return 0;
 }
-
-
